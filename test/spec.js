66a15462868625c81a0ac59d5e21e1992c21660d
Mikulas Sychra <mikulas.sychra@gmail.com>
2017-06-01 20:38:53 +0200

    first working test

--- test/spec.js
+++ test/spec.js
@@ -4,15 +4,15 @@ var env = require('./environment.js');
 describe('pages with login', function() {
   it('should log in with a non-Angular page', function() {
 
+	  browser.waitForAngularEnabled(false);
 
-	
+    browser.get(env.baseUrl + '/app/');
 
-	browser.waitForAngularEnabled(false);
+    browser.waitForAngularEnabled(true);
 
-    browser.get(env.baseUrl + '/');
+    expect(element(by.css('div.page-footer-inner')).getText()).toEqual('2017 © THiNX');
 
-    var angularElement = element(by.css('.page-title'));
-    expect(angularElement.getText()).toEqual('Dashboard');
+    //expect(rootScope.profile.owner.toBe('eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f'));
 
     // Make sure the cookie is still set.
     //browser.manage().getCookie('testcookie').then(function(cookie) {
