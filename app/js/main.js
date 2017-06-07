e00351438f4732ac5c148c08fdecea664a4758b6
Mikuláš Sychra <mikulas.sychra@t-mobile.cz>
2017-06-07 13:04:41 +0200

    Removed duplicate close

--- app/js/main.js
+++ app/js/main.js
@@ -111,6 +111,7 @@ RTM.controller('AppController', ['$scope', '$rootScope', 'webNotification', 'Rol
     $scope.$on('$viewContentLoaded', function() {
         //App.initComponents(); // init core components
         //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive 
+
     });
 
     console.log(' === ROOT === ');
